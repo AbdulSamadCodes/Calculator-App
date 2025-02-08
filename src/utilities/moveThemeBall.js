0203ac6734e7a513156167c7a30c2a59037e8a16
Abdul Samad <samadcom797@gmail.com>
2025-02-08 15:45:21 +0500

    Interface design completed

--- src/utilities/moveThemeBall.js
+++ src/utilities/moveThemeBall.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { useLayoutEffect } from 'react';
 
 function moveSlideBall(themePallete, currentTheme, ballRef) {
-  
-  useLayoutEffect(() => {
+  const setBallPosition = () => {
     const themeBall = ballRef?.current;
     const OFFSET = 5;
 
     const themes = Object.keys(themePallete);
     const themeIndex = themes.findIndex(theme => theme === currentTheme);
-    
+
     const position = themeBall.parentElement.previousElementSibling
       .children[themeIndex].offsetLeft;
 
     themeBall.style.left = `${position + OFFSET}px`;
-  },[currentTheme]);
+  }
+
+  useLayoutEffect(() => {
+    setBallPosition();
+  }, [currentTheme]);
 }
 
 export { moveSlideBall };
